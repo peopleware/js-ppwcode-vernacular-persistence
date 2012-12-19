65a4469472b610b5cc275d4c769b6b6d70961cef
Jan Dockx <Jan_Dockx@peopleware.be>
2012-12-19 10:24:32 +0000

    IdNotFoundException now is a SemanticException
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/persistence/trunk@6500 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/persistence/IdNotFoundException.js
+++ src/persistence/IdNotFoundException.js
@@ -1,7 +1,7 @@
-define(["dojo/_base/declare"],
-    function(declare) {
+define(["dojo/_base/declare", "ppwcode/exceptions/SemanticException"],
+    function(declare, SemanticException) {
 
-      var IdNotFoundException = declare("be.ppwcode.vernacular.persistence.IdNotFoundException", [], {
+      var IdNotFoundException = declare("be.ppwcode.vernacular.persistence.IdNotFoundException", [SemanticException], {
 
         // MUDO
 
