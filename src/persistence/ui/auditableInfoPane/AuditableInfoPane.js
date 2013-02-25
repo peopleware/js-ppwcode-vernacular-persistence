7db22aa873d32e4e636e90a0c3d2be0a97e03c53
Jan Dockx <Jan_Dockx@peopleware.be>
2013-02-25 22:14:42 +0000

    Demonstration and cleanup of AuditableInfoPane
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/persistence/trunk@6565 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/persistence/ui/auditableInfoPane/AuditableInfoPane.js
+++ src/persistence/ui/auditableInfoPane/AuditableInfoPane.js
@@ -1,15 +1,15 @@
 define(["dojo/_base/declare",
-        "ppwcode/semantics/ui/_SemanticObjectPane",
+        "ppwcode/semantics/ui/_SemanticObjectPane", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin",
         "dojo/text!./auditableInfoPane.html", "dojo/i18n!./nls/labels",
         "ppwcode/persistence/AuditableObject",
         "dojox/mvc/Output", "dojox/mvc/at", "dojo/date/locale",
         "xstyle/css!./auditableInfoPane.css"],
   function(declare,
-           _SemanticObjectPane,
+           _SemanticObjectPane, _TemplatedMixin, _WidgetsInTemplateMixin,
            template, labels,
            AuditableObject) {
 
-    return declare([_SemanticObjectPane], {
+    return declare([_SemanticObjectPane, _TemplatedMixin, _WidgetsInTemplateMixin], {
       // summary:
       //   This widget shows, read-only, the audit information from an `AuditableObject`,
       //   low key.
