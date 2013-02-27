b8c3a2393d1a299db79c2cc04289c63dc764faec
Jan Dockx <Jan_Dockx@peopleware.be>
2013-02-27 13:18:14 +0000

    Removed obsolete temp date handling. Let's try this on the server.
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/persistence/trunk@6578 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/persistence/AuditableObject.js
+++ src/persistence/AuditableObject.js
@@ -1,36 +1,16 @@
 define(["dojo/_base/declare", "./PersistentObject", "dojo/date"],
     function(declare, PersistentObject, dojoDate) {
 
-      function getDate(dateString) {
-        //MUDO for now we parse the date we get from the JSON but we should provide a correct date format in the JSON
-        // so we can just call new Date(json.dateString);
-        if (dateString === null) {
-          return null;
-        }
-        if (Object.prototype.toString.call(dateString) === '[object Date]') {
-          // dateString is already a valid Date
-          return dateString;
-        }
-        var year = dateString.slice(0, 4);
-        var month = dateString.slice(5, 7) - 1;
-        var day = dateString.slice(8, 10);
-        var hour = dateString.slice(11, 13);
-        var minute = dateString.slice(14, 16);
-        var second = dateString.slice(17, 19);
-
-        return new Date(year, month, day, hour, minute, second);
-      }
-
       function internalReload(/*AuditableObject*/ self, /*Object*/ json) {
         if (json && json.createdAt && json.createdBy && json.lastModifiedAt && json.lastModifiedBy /* TODO json.... undefined, but not null */) {
           // all data or nothing;
           // TODO add precondition for this
           if ((self.createdAt || self.createdBy) &&
-              ((self.createdAt && (dojoDate.compare(getDate(json.createdAt), self.createdAt) !== 0)) ||
+              ((self.createdAt && (dojoDate.compare(json.createdAt, self.createdAt) !== 0)) ||
                (self.createdBy && json.createdBy != self.createdBy))) {
             throw "ERROR cannot change from existing created information"; // TODO precondition
           }
-          if (self.lastModifiedAt && getDate(json.lastModifiedAt) < self.lastModifiedAt) {
+          if (self.lastModifiedAt && json.lastModifiedAt < self.lastModifiedAt) {
             throw "ERROR cannot become an earlier modified version"; // TODO precondition
           }
           if (self.lastModifiedBy && !json.lastModifiedBy) {
@@ -39,12 +19,12 @@ define(["dojo/_base/declare", "./PersistentObject", "dojo/date"],
           // this will happen with the JSON response from a creation or update, and during construction
           if (! self.createdBy) {
             //noinspection JSUnresolvedFunction
-            self._changeAttrValue("createdAt", getDate(json.createdAt));
+            self._changeAttrValue("createdAt", json.createdAt);
             //noinspection JSUnresolvedFunction
             self._changeAttrValue("createdBy", json.createdBy);
           }
           //noinspection JSUnresolvedFunction
-          self._changeAttrValue("lastModifiedAt", getDate(json.lastModifiedAt));
+          self._changeAttrValue("lastModifiedAt", json.lastModifiedAt);
           //noinspection JSUnresolvedFunction
           self._changeAttrValue("lastModifiedBy", json.lastModifiedBy);
         }
