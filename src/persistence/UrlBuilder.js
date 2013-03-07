62e1a3c244f72ea207edacbfb0b8514fa80b15b4
Jan Dockx <Jan_Dockx@peopleware.be>
2013-03-07 15:01:22 +0000

    Working … this looks ok.
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/persistence/trunk@6632 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/persistence/UrlBuilder.js
+++ src/persistence/UrlBuilder.js
@@ -1,7 +1,7 @@
-define(["dojo/_base/declare", "dojo/_base/lang"],
-    function(declare, lang) {
+define(["dojo/_base/declare", "ppwcode/oddsAndEnds/typeOf"],
+    function(declare, typeOf) {
 
-      var UrlBuilder = declare([PersistentObject], {
+      var UrlBuilder = declare([], {
         // summary:
         //   An interface for which you have to provide an implementation.
         //   Instances, when operational, return url's for CRUD functions,
@@ -11,21 +11,81 @@ define(["dojo/_base/declare", "dojo/_base/lang"],
         _c_invar: [
         ],
 
-        retrieve: function(serverType, id) {
-          // summary:
-          //   Returns a URL to retrieve an object of the given `serverType` with the given id.
+        get: function(/*String*/ method) {
+          this._c_pre(function() {return typeOf(method) === "string";});
 
-          this._c_pre(function() {return serverType && lang.isString(serverType);});
-          this._c_pre(function() {return id && lang.isNumber(id);});
+          switch (method) {
+            case "GET":
+              return this.retrieve;
+            case "POST":
+              return this.create;
+            case "PUT":
+              return this.update;
+            case "DELETE":
+              return this.delete;
+            default:
+              throw new Error("Unknown method: " + method);
+          }
+        },
+
+        search: function(/*String?*/ serverType, /*Object?*/ query) {
+          // summary:
+          //   Returns a URL to search for objects.
+          // description:
+          //   A search for a specific `serverType` with or without a `query` should
+          //   return objects of that type.
+          //   A search for a specific `serverType` without a `query` should return all
+          //   objects of that type.
+          //   The semantics of a search without a specific `serverType`, with
+          //   or without a `query`, is open.
+          //   The semantics of the `query`, is open.
+          this._c_pre(function() {return !serverType || typeOf(serverType) === "string";});
+          this._c_pre(function() {return !query || typeOf(query) === "object";});
 
           this._c_ABSTRACT();
         },
 
-        create: function(serverType, id) {
+        toMany: function(serverType, id, serverPropertyName) {
+          // summary:
+          //   Returns a URL to retrieve all objects of a to-many property of an object.
+          this._c_pre(function() {return typeOf(serverType) === "string";});
+          this._c_pre(function() {return typeOf(id) === "number";});
+          this._c_pre(function() {return typeOf(serverPropertyName) === "string";});
+
+          this._c_ABSTRACT();
+        },
+
+        retrieve: function(serverType, id) {
+          // summary:
+          //   Returns a URL to retrieve an object of the given `serverType` with the given id.
+          this._c_pre(function() {return typeOf(serverType) === "string";});
+          this._c_pre(function() {return typeOf(id) === "number";});
+
+          this._c_ABSTRACT();
+        },
+
+        create: function(serverType) {
           // summary:
           //   Returns a URL to create an object of the given `serverType`.
+          this._c_pre(function() {return typeOf(serverType) === "string";});
 
-          this._c_pre(function() {return serverType && lang.isString(serverType);});
+          this._c_ABSTRACT();
+        },
+
+        update: function(serverType, id) {
+          // summary:
+          //   Returns a URL to update an object of the given `serverType`.
+          this._c_pre(function() {return typeOf(serverType) === "string";});
+          this._c_pre(function() {return typeOf(id) === "number";});
+
+          this._c_ABSTRACT();
+        },
+
+        delete: function(serverType, id) {
+          // summary:
+          //   Returns a URL to delete an object of the given `serverType`.
+          this._c_pre(function() {return typeOf(serverType) === "string";});
+          this._c_pre(function() {return typeOf(id) === "number";});
 
           this._c_ABSTRACT();
         }
