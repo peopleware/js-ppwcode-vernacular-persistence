7cab411c814ba88187abc52bca0246b97a822d2e
Jan Dockx <Jan_Dockx@peopleware.be>
2013-03-10 20:08:03 +0000

    more tests; GREEN
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/persistence/trunk@6655 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/persistence/test/_Cache.js
+++ src/persistence/test/_Cache.js
@@ -46,6 +46,8 @@ define(["dojo/main", "ppwcode/contracts/doh",
             doh.is(this.person, tracked);
             tracked = this.subject.getPo(this.person);
             doh.is(this.person, tracked);
+            // IDEA test breaks encapsulation -- whenever this fails, just remove the test
+            doh.is(1, this.subject._data[this.person.get("persistenceType") + "@" + personId].getNrOfReferers());
             console.log(JSON.stringify(this.subject.report()));
           },
           tearDown: function() {
@@ -68,11 +70,109 @@ define(["dojo/main", "ppwcode/contracts/doh",
             doh.is(this.person, tracked);
             tracked = this.subject.getPo(this.person);
             doh.is(this.person, tracked);
+            // IDEA test breaks encapsulation -- whenever this fails, just remove the test
+            doh.is(3, this.subject._data[this.person.get("persistenceType") + "@" + personId].getNrOfReferers());
             console.log(JSON.stringify(this.subject.report()));
           },
           tearDown: function() {
             delete this.person;
             delete this.subject;
+          }
+        },
+
+        {
+          name: "trackPo quatro",
+          setUp: function() {
+            this.person = createPerson();
+            this.subject = new _Cache();
+            this.referer1 = {};
+            this.referer2 = {};
+          },
+          runTest: function() {
+            this.subject.trackPo(this.person, this);
+            this.subject.trackPo(this.person, this.referer1);
+            this.subject.trackPo(this.person, this.referer1);
+            this.subject.trackPo(this.person, this.referer2);
+            var tracked = this.subject.getPoByTypeAndId(this.person.get("persistenceType"), personId);
+            doh.is(this.person, tracked);
+            tracked = this.subject.getPo(this.person);
+            doh.is(this.person, tracked);
+            // IDEA test breaks encapsulation -- whenever this fails, just remove the test
+            doh.is(3, this.subject._data[this.person.get("persistenceType") + "@" + personId].getNrOfReferers());
+            console.log(JSON.stringify(this.subject.report()));
+          },
+          tearDown: function() {
+            delete this.person;
+            delete this.subject;
+            delete this.referer1;
+            delete this.referer2;
+          }
+        },
+
+        {
+          name: "stopTracking",
+          setUp: function() {
+            this.person = createPerson();
+            this.subject = new _Cache();
+            this.referer1 = {};
+            this.referer2 = {};
+          },
+          runTest: function() {
+            this.subject.trackPo(this.person, this);
+            this.subject.trackPo(this.person, this.referer1);
+            this.subject.trackPo(this.person, this.referer1);
+            this.subject.trackPo(this.person, this.referer2);
+            var tracked = this.subject.getPoByTypeAndId(this.person.get("persistenceType"), personId);
+            doh.is(this.person, tracked);
+            // IDEA test breaks encapsulation -- whenever this fails, just remove the test
+            doh.is(3, this.subject._data[this.person.get("persistenceType") + "@" + personId].getNrOfReferers());
+            console.log(JSON.stringify(this.subject.report()));
+
+            this.subject.stopTracking(this.person, {});
+            tracked = this.subject.getPoByTypeAndId(this.person.get("persistenceType"), personId);
+            doh.is(this.person, tracked);
+            // IDEA test breaks encapsulation -- whenever this fails, just remove the test
+            doh.is(3, this.subject._data[this.person.get("persistenceType") + "@" + personId].getNrOfReferers());
+            console.log(JSON.stringify(this.subject.report()));
+
+            this.subject.stopTracking(this.person, this.referer1);
+            tracked = this.subject.getPoByTypeAndId(this.person.get("persistenceType"), personId);
+            doh.is(this.person, tracked);
+            // IDEA test breaks encapsulation -- whenever this fails, just remove the test
+            doh.is(2, this.subject._data[this.person.get("persistenceType") + "@" + personId].getNrOfReferers());
+            console.log(JSON.stringify(this.subject.report()));
+
+            this.subject.stopTracking(this.person, this.referer2);
+            tracked = this.subject.getPoByTypeAndId(this.person.get("persistenceType"), personId);
+            doh.is(this.person, tracked);
+            // IDEA test breaks encapsulation -- whenever this fails, just remove the test
+            doh.is(1, this.subject._data[this.person.get("persistenceType") + "@" + personId].getNrOfReferers());
+            console.log(JSON.stringify(this.subject.report()));
+
+            this.subject.trackPo(this.person, this.referer1);
+            tracked = this.subject.getPoByTypeAndId(this.person.get("persistenceType"), personId);
+            doh.is(this.person, tracked);
+            // IDEA test breaks encapsulation -- whenever this fails, just remove the test
+            doh.is(2, this.subject._data[this.person.get("persistenceType") + "@" + personId].getNrOfReferers());
+            console.log(JSON.stringify(this.subject.report()));
+
+            this.subject.stopTracking(this.person, this.referer1);
+            tracked = this.subject.getPoByTypeAndId(this.person.get("persistenceType"), personId);
+            doh.is(this.person, tracked);
+            // IDEA test breaks encapsulation -- whenever this fails, just remove the test
+            doh.is(1, this.subject._data[this.person.get("persistenceType") + "@" + personId].getNrOfReferers());
+            console.log(JSON.stringify(this.subject.report()));
+
+            this.subject.stopTracking(this.person, this);
+            tracked = this.subject.getPoByTypeAndId(this.person.get("persistenceType"), personId);
+            doh.f(tracked);
+            console.log(JSON.stringify(this.subject.report()));
+          },
+          tearDown: function() {
+            delete this.person;
+            delete this.subject;
+            delete this.referer1;
+            delete this.referer2;
           }
         }
 
