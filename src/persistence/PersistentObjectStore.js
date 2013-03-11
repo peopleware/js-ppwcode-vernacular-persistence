eb0627fdddb01e06e3b5eef74a7426b508356858
Jan Dockx <Jan_Dockx@peopleware.be>
2013-03-11 00:32:35 +0000

    keys for PersistentObjectStore
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/persistence/trunk@6664 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/persistence/PersistentObjectStore.js
+++ src/persistence/PersistentObjectStore.js
@@ -1,7 +1,9 @@
 define(["dojo/_base/declare", "ppwcode/collections/StoreOfStateful",
-        "./PersistentObject"],
+        "./PersistentObject",
+        "dojo/_base/lang"],
   function(declare, StoreOfStateful,
-           PersistentObject) {
+           PersistentObject,
+           lang) {
 
     var PersistentObjectStore = declare([StoreOfStateful], {
 
@@ -14,6 +16,49 @@ define(["dojo/_base/declare", "ppwcode/collections/StoreOfStateful",
       }
 
     });
+
+
+
+    PersistentObjectStore.keyForId = function(/*Function*/ Constructor, /*Number*/ id, /*String*/ toManyPropertyName) {
+      // summary:
+      //   When PersistentObjectStore is used to express a to-many reference
+      //   of a PersistentObject, this returns a unique key for such a store.
+      // IDEA can't use current form of precondition here
+      if (! (Constructor && lang.isFunction(Constructor))) {
+        throw new Error("precondition violation: Constructor && lang.isFunction(Constructor)");
+      }
+      // IDEA must be a subtype of PeristentObject
+      if (! (Constructor.prototype.persistenceType)) {
+        throw new Error("precondition violation: Constructor.prototype.persistenceType");
+      }
+      if (! (id)) {
+        throw new Error("precondition violation: id");
+      }
+      if (! (toManyPropertyName && lang.isString(toManyPropertyName))) {
+        throw new Error("precondition violation: toManyPropertyName && lang.isString(toManyPropertyName)");
+      }
+
+      var result = PersistentObject.keyForId(Constructor, id) + "/" + toManyPropertyName;
+      return result; // return String
+    };
+
+    PersistentObjectStore.keyForObject = function(/*PersistentObject*/ po, /*String*/ toManyPropertyName) {
+      // summary:
+      //   When PersistentObjectStore is used to express a to-many reference
+      //   of a PersistentObject, this returns a unique key for such a store.
+      // IDEA can't use current form of precondition here
+      if (! (po && po.get("persistenceId") != null)) {
+        throw new Error("precondition violation: po && po.get('persistenceId') != null");
+      }
+      if (! (toManyPropertyName && lang.isString(toManyPropertyName))) {
+        throw new Error("precondition violation: toManyPropertyName && lang.isString(toManyPropertyName)");
+      }
+
+      var Constructor = po.constructor;
+      return PersistentObjectStore.keyForId(Constructor, po.get("persistenceId"), toManyPropertyName); // return String
+    };
+
+
 
     return PersistentObjectStore;
   }
--- src/persistence/test/PersistentObjectStore.js
+++ src/persistence/test/PersistentObjectStore.js
@@ -1,13 +1,37 @@
 define(["dojo/main", "ppwcode/contracts/doh",
-        "../PersistentObjectStore"],
+        "../PersistentObjectStore", "../PersistentObject",
+        "dojo/_base/declare", "dojo/_base/lang"],
   function(dojo, doh,
-           PersistentObjectStore) {
+           PersistentObjectStore, PersistentObject,
+           declare, lang) {
+
+    var type = "SOME TYPE DESCRIPTOR";
+    var Mock = declare([PersistentObject], {
+      persistenceType: type
+    });
 
     doh.register("PersistentObjectStore", [
 
       function testConstructor() {
         var subject = new PersistentObjectStore();
         doh.invars(subject);
+      },
+
+      function testKeyForId() {
+        var id = 9859893;
+        var toMany = "A TOO MANY NAME";
+        var key = PersistentObjectStore.keyForId(Mock, id, toMany);
+        doh.t(lang.isString(key));
+        doh.is(type + "@" + id + "/" + toMany, key);
+      },
+
+      function testKeyForObject() {
+        var id = 9859893;
+        var subject = new Mock({persistenceId: id});
+        var toMany = "A TOO MANY NAME";
+        var key = PersistentObjectStore.keyForObject(subject, toMany);
+        doh.t(lang.isString(key));
+        doh.is(type + "@" + id + "/" + toMany, key);
       }
 
     ]);
