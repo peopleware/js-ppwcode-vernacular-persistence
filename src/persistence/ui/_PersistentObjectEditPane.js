313a3a5909036db9687b90536a34aaf6bd084845
Jan Dockx <Jan_Dockx@peopleware.be>
2013-12-03 13:15:22 +0000

    * _setVisible available to subclasses * save/cancel/remove return their Promise
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/persistence/trunk@7319 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/persistence/ui/_PersistentObjectEditPane.js
+++ src/persistence/ui/_PersistentObjectEditPane.js
@@ -115,15 +115,16 @@ define(["dojo/_base/declare", "ppwcode-vernacular-semantics/ui/_semanticObjectPa
         if (!po || !po.get("persistenceId")) {
           var closer = self.get("closer");
           closer();
-          return;
+          return po;
         }
         self.set("presentationMode", self.BUSY);
         var refresher = self.get("refresher");
         if (refresher) {
           var refreshPromise = refresher(po);
           refreshPromise.then(
-            function() {
+            function(result) {
               self.set("presentationMode", self.VIEW);
+              return result;
             },
             function(e) {
               // this is not really a fatal error, but an inconvenience
@@ -143,8 +144,13 @@ define(["dojo/_base/declare", "ppwcode-vernacular-semantics/ui/_semanticObjectPa
               }
               var closer = self.get("closer");
               closer();
+              throw e;
             }
           );
+          return refreshPromise;
+        }
+        else {
+          return po;
         }
       },
 
@@ -181,19 +187,20 @@ define(["dojo/_base/declare", "ppwcode-vernacular-semantics/ui/_semanticObjectPa
               }
               // MUDO workaround https://projects.peopleware.be/jira44/browse/PICTOPERFECT-505
               // The server PUT result is not correct! We retrieve extra, to get the correct result for now!
-              self.cancel(); // yes, weird, but it does the trick for now for the workaround
-              return;
+              return self.cancel(); // yes, weird, but it does the trick for now for the workaround
             }
             if (persisterName === "creator") {
               // we need to switch the old target with the result
               self.set("target", result);
               self.set("presentationMode", self.VIEW);
             }
+            return result;
           },
           function(exc) {
             self._handleSaveException(exc);
           }
-        )
+        );
+        return persistPromise;
       },
 
       remove: function() {
@@ -218,6 +225,7 @@ define(["dojo/_base/declare", "ppwcode-vernacular-semantics/ui/_semanticObjectPa
             self.set("presentationMode", self.VIEW);
             var closer = self.get("closer");
             closer();
+            return result;
           },
           function(e) {
             if (e.isInstanceOf && e.isInstanceOf(SemanticException)) {
@@ -242,8 +250,10 @@ define(["dojo/_base/declare", "ppwcode-vernacular-semantics/ui/_semanticObjectPa
             self.set("presentationMode", self.ERROR);
             alert(e);
             self.cancel();
+            throw e;
           }
-        )
+        );
+        return deletePromise;
       },
 
       _handleSaveException: function(exc) {
--- src/persistence/ui/persistentObjectButtonEditPane/PersistentObjectButtonEditPane.js
+++ src/persistence/ui/persistentObjectButtonEditPane/PersistentObjectButtonEditPane.js
@@ -24,13 +24,187 @@ define(["dojo/_base/declare", "dojo/dom-style",
         "ppwcode-vernacular-persistence/ui/auditableInfoPane/AuditableInfoPane",
         "dijit/form/Button",
         "xstyle/css!./PersistentObjectButtonEditPane.css"],
-    function(declare, domStyle,
-             LayoutContainer, _PersistentObjectEditPane, _TemplatedMixin, _WidgetsInTemplateMixin,
-             template, labels,
-             PersistentObject, AuditableObject,
-             logger) {
+  function(declare, domStyle,
+           LayoutContainer, _PersistentObjectEditPane, _TemplatedMixin, _WidgetsInTemplateMixin,
+           template, labels,
+           PersistentObject, AuditableObject,
+           logger) {
 
-      function setVisible(/*Button*/ button, /*Boolean*/ condition, /*Boolean*/ busy) {
+    return declare([LayoutContainer, _PersistentObjectEditPane, _TemplatedMixin, _WidgetsInTemplateMixin], {
+      // summary:
+      //    Widget that extends _PersistentObjectEditPane with an actual presentation.
+      //    There are buttons to control the edit cycle on the bottom of the pane.
+      //    The content will show a scrollbar if it is to high or wide.
+      // description:
+      //    This is a ./_PersistentObjectEditPane.
+      //    The widget shows buttons to go the edit mode, cancel or save an edit,
+      //    and delete the persistent object.
+      //    The actual actions are done by a refresher, creator, saver, and remover
+      //    function.
+      //    Intended to wrap around a _SemanticObjectPane, which can be injected with
+      //    set("contentPane").
+      //    If this contentPane has a css width, it is copied to this when set.
+      //
+      //    On any change of the target, we reevaluate the button display.
+
+      templateString: template,
+      labels: labels,
+
+      design: "headline",
+
+      getTargetType: function() {
+        // summary:
+        //    PersistentObject if there is no persistentObjectDetail. The target type of
+        //    the persistentObjectPane otherwise.
+        return this.persistentObjectPane ? this.persistentObjectPane.getTargetType() : PersistentObject;
+      },
+
+      // persistentObjectPane: ppwcode/semantics/_SemanticObjectPane
+      //    An _SemanticObjectPane that displays the target.
+      persistentObjectPane: null,
+
+      _contentPane: null,
+      _errorPane: null,
+      _auditableInfo: null,
+      _btnEdit: null,
+      _btnCancel: null,
+      _btnSave: null,
+      _btnDelete: null,
+
+      _poListener: null,
+
+      postCreate: function() {
+        this._setButtonsStyles(this.NOTARGET);
+        if (! this.get("target")) { // TODO is this really necessary? why? write a comment
+          this.set("target", null);
+        }
+      },
+
+      destroy: function() {
+        this._beingDestroyed = true;
+        this.set("target", null);
+        // will also destroy _auditableInfo, if there still is one
+        this.inherited(arguments);
+      },
+
+      _wrappedDetails: function() {
+        var result = [];
+        if (this.persistentObjectPane) {
+          result.push(this.persistentObjectPane);
+        }
+        result.push(this._auditableInfo);
+        return result;
+      },
+
+      _propagateTarget: function(/*PersistentObject*/ po) {
+        // summary:
+        //    Set the target on the persistentObjectPane and _auditableInfo
+
+        var self = this;
+        if (self.get("persistentObjectPane")) {
+          self.get("persistentObjectPane").set("target", po);
+          if (self.opener) {
+            self.get("persistentObjectPane").set("opener", self.opener);
+          }
+        }
+        var ao = po && po.isInstanceOf(AuditableObject) ? po : null;
+        // TODO invisible if there is no target
+        self._auditableInfo.set("target", ao);
+        if (self._poListener) {
+          self._poListener.remove();
+          self._poListener = null;
+        }
+        if (po) {
+          self._poListener = po.watch(function() {
+            self._setButtonsStyles(self.get("stylePresentationMode"));
+          });
+          self.own(self._poListener);
+        }
+      },
+
+      _setOpenerAttr: function(opener) {
+        this.opener = opener;
+        if (this.get("persistentObjectPane")) {
+          this.get("persistentObjectPane").set("opener", this.opener);
+        }
+      },
+
+      _setPersistentObjectPaneAttr: function(poPane) {
+        // summary:
+        //    Set the persistentObjectPane. If it has a target, and we have no target,
+        //    that becomes are new target.
+        // description:
+        //    Note that the old persistentObjectPane was not created by us,
+        //    and thus also is not destroyed by us!
+
+        // TODO poDetail target type is a PersistentObject
+
+        // might be called implicity by constructor of WidgetBase
+        var oldPoPane = this.get("persistentObjectPane");
+        if (oldPoPane) {
+          oldPoPane.set("target", null);
+          // no destroy of oldPoDetail; we didn't create it; if we do, we have to!
+          this._contentPane.removeChild(oldPoPane);
+          oldPoPane.destroyRecursive();
+        }
+        this._set("persistentObjectPane", poPane);
+        if (poPane) {
+          var poPaneWidth = domStyle.get(poPane.domNode, "width");
+          if (!poPaneWidth) {
+            logger.info("poPane has no width set.");
+          }
+          else {
+            domStyle.set(this.domNode, "width", (poPaneWidth + 16) + "px"); // see css: 16px padding
+          }
+          this._contentPane.addChild(poPane);
+          // new detail might already have a target
+          var ourPo = this.get("target");
+          var wrappedPo = poPane.get("target");
+          if (ourPo) {
+            poPane.set("target", ourPo);
+          }
+          else if (wrappedPo) {
+            this.set("target", wrappedPo);
+          }
+          // else we both have null targets; ok
+        }
+        this.resize();
+      },
+
+      validate: function() {
+        // TODO this is not functional in this version
+        // TODO: standard Widget function?
+        if (this.get("presentationMode") === this.EDIT) {
+          var poPane = this.get("persistentObjectPane");
+          if (poPane && poPane.validate) {
+            return poPane.validate();
+          }
+          else {
+            return true;
+          }
+        }
+        else {
+          return true;
+        }
+      },
+
+      _setButtonsStyles: function(stylePresentationMode) {
+        // Set the button style.
+        this._c_pre(function() { return stylePresentationMode && this.stylePresentationModes.indexOf(stylePresentationMode) >= 0; });
+
+        var po = this.get("target");
+        var busy = (stylePresentationMode === this.BUSY);
+        this._setVisible(this._btnEdit, stylePresentationMode === this.VIEW && (po.get("editable") || po.get("deletable")), false);
+        this._setVisible(this._btnCancel, this.isInEditMode(), busy);
+        this._setVisible(this._btnDelete, this.isInEditMode() && po.get("deletable") && po.get("persistenceId"), busy);
+        this._setVisible(this._btnSave, this.isInEditMode() && (po.get("editable") || !po.get("persistenceId")), busy);
+      },
+
+      _localPresentationModeChange: function(presentationMode) {
+        this._setButtonsStyles(this.get("stylePresentationMode"));
+      },
+
+      _setVisible: function (/*Button*/ button, /*Boolean*/ condition, /*Boolean*/ busy) {
         // IDEA use FX
         if (button && button.domNode) {
           var displayStyle = condition ? "inline-block" : "none";
@@ -40,180 +214,6 @@ define(["dojo/_base/declare", "dojo/dom-style",
         // TODO should listen to editable and deletable
       }
 
-      return declare([LayoutContainer, _PersistentObjectEditPane, _TemplatedMixin, _WidgetsInTemplateMixin], {
-        // summary:
-        //    Widget that extends _PersistentObjectEditPane with an actual presentation.
-        //    There are buttons to control the edit cycle on the bottom of the pane.
-        //    The content will show a scrollbar if it is to high or wide.
-        // description:
-        //    This is a ./_PersistentObjectEditPane.
-        //    The widget shows buttons to go the edit mode, cancel or save an edit,
-        //    and delete the persistent object.
-        //    The actual actions are done by a refresher, creator, saver, and remover
-        //    function.
-        //    Intended to wrap around a _SemanticObjectPane, which can be injected with
-        //    set("contentPane").
-        //    If this contentPane has a css width, it is copied to this when set.
-        //
-        //    On any change of the target, we reevaluate the button display.
+  });
 
-        templateString: template,
-        labels: labels,
-
-        design: "headline",
-
-        getTargetType: function() {
-          // summary:
-          //    PersistentObject if there is no persistentObjectDetail. The target type of
-          //    the persistentObjectPane otherwise.
-          return this.persistentObjectPane ? this.persistentObjectPane.getTargetType() : PersistentObject;
-        },
-
-        // persistentObjectPane: ppwcode/semantics/_SemanticObjectPane
-        //    An _SemanticObjectPane that displays the target.
-        persistentObjectPane: null,
-
-        _contentPane: null,
-        _errorPane: null,
-        _auditableInfo: null,
-        _btnEdit: null,
-        _btnCancel: null,
-        _btnSave: null,
-        _btnDelete: null,
-
-        _poListener: null,
-
-        postCreate: function() {
-          this._setButtonsStyles(this.NOTARGET);
-          if (! this.get("target")) { // TODO is this really necessary? why? write a comment
-            this.set("target", null);
-          }
-        },
-
-        destroy: function() {
-          this._beingDestroyed = true;
-          this.set("target", null);
-          // will also destroy _auditableInfo, if there still is one
-          this.inherited(arguments);
-        },
-
-        _wrappedDetails: function() {
-          var result = [];
-          if (this.persistentObjectPane) {
-            result.push(this.persistentObjectPane);
-          }
-          result.push(this._auditableInfo);
-          return result;
-        },
-
-        _propagateTarget: function(/*PersistentObject*/ po) {
-          // summary:
-          //    Set the target on the persistentObjectPane and _auditableInfo
-
-          var self = this;
-          if (self.get("persistentObjectPane")) {
-            self.get("persistentObjectPane").set("target", po);
-            if (self.opener) {
-              self.get("persistentObjectPane").set("opener", self.opener);
-            }
-          }
-          var ao = po && po.isInstanceOf(AuditableObject) ? po : null;
-          // TODO invisible if there is no target
-          self._auditableInfo.set("target", ao);
-          if (self._poListener) {
-            self._poListener.remove();
-            self._poListener = null;
-          }
-          if (po) {
-            self._poListener = po.watch(function() {
-              self._setButtonsStyles(self.get("stylePresentationMode"));
-            });
-            self.own(self._poListener);
-          }
-        },
-
-        _setOpenerAttr: function(opener) {
-          this.opener = opener;
-          if (this.get("persistentObjectPane")) {
-            this.get("persistentObjectPane").set("opener", this.opener);
-          }
-        },
-
-        _setPersistentObjectPaneAttr: function(poPane) {
-          // summary:
-          //    Set the persistentObjectPane. If it has a target, and we have no target,
-          //    that becomes are new target.
-          // description:
-          //    Note that the old persistentObjectPane was not created by us,
-          //    and thus also is not destroyed by us!
-
-          // TODO poDetail target type is a PersistentObject
-
-          // might be called implicity by constructor of WidgetBase
-          var oldPoPane = this.get("persistentObjectPane");
-          if (oldPoPane) {
-            oldPoPane.set("target", null);
-            // no destroy of oldPoDetail; we didn't create it; if we do, we have to!
-            this._contentPane.removeChild(oldPoPane);
-            oldPoPane.destroyRecursive();
-          }
-          this._set("persistentObjectPane", poPane);
-          if (poPane) {
-            var poPaneWidth = domStyle.get(poPane.domNode, "width");
-            if (!poPaneWidth) {
-              logger.info("poPane has no width set.");
-            }
-            else {
-              domStyle.set(this.domNode, "width", (poPaneWidth + 16) + "px"); // see css: 16px padding
-            }
-            this._contentPane.addChild(poPane);
-            // new detail might already have a target
-            var ourPo = this.get("target");
-            var wrappedPo = poPane.get("target");
-            if (ourPo) {
-              poPane.set("target", ourPo);
-            }
-            else if (wrappedPo) {
-              this.set("target", wrappedPo);
-            }
-            // else we both have null targets; ok
-          }
-          this.resize();
-        },
-
-        validate: function() {
-          // TODO this is not functional in this version
-          // TODO: standard Widget function?
-          if (this.get("presentationMode") === this.EDIT) {
-            var poPane = this.get("persistentObjectPane");
-            if (poPane && poPane.validate) {
-              return poPane.validate();
-            }
-            else {
-              return true;
-            }
-          }
-          else {
-            return true;
-          }
-        },
-
-        _setButtonsStyles: function(stylePresentationMode) {
-          // Set the button style.
-          this._c_pre(function() { return stylePresentationMode && this.stylePresentationModes.indexOf(stylePresentationMode) >= 0; });
-
-          var po = this.get("target");
-          var busy = (stylePresentationMode === this.BUSY);
-          setVisible(this._btnEdit, stylePresentationMode === this.VIEW && (po.get("editable") || po.get("deletable")), false);
-          setVisible(this._btnCancel, this.isInEditMode(), busy);
-          setVisible(this._btnDelete, this.isInEditMode() && po.get("deletable") && po.get("persistenceId"), busy);
-          setVisible(this._btnSave, this.isInEditMode() && (po.get("editable") || !po.get("persistenceId")), busy);
-        },
-
-        _localPresentationModeChange: function(presentationMode) {
-          this._setButtonsStyles(this.get("stylePresentationMode"));
-        }
-
-      });
-
-    });
+});
