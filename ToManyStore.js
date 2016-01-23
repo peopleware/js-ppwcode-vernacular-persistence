347d52030a12919c27166033001075de654dfbe5
Jan Dockx <Jan_Dockx@peopleware.be>
2016-01-23 14:28:59 +0100

    ToManyStore now is an IdentifiableObject. But for that to be possible, it needs a key, and for that to be possible, it needs to borrow it somewhere, and we introduced the (mandatory) one property.
    ToManyStore now also has a mid.
    
    This change is not backward compatible.

--- ToManyStore.js
+++ ToManyStore.js
@@ -14,10 +14,16 @@
  limitations under the License.
 */
 
-define(["dojo/_base/declare", "./PersistentObjectStore", "dojo/Stateful", "ppwcode-util-oddsAndEnds/js", "ppwcode-util-oddsAndEnds/promise/Arbiter"],
-  function(declare, PersistentObjectStore, Stateful, js, Arbiter) {
+define(["dojo/_base/declare",
+        "./PersistentObjectStore", "ppwcode-vernacular-semantics/IdentifiableObject", "dojo/Stateful",
+        "./PersistentObject",
+        "ppwcode-util-oddsAndEnds/js", "ppwcode-util-oddsAndEnds/promise/Arbiter", "module"],
+  function(declare,
+           PersistentObjectStore, IdentifiableObject, Stateful,
+           PersistentObject,
+           js, Arbiter, module) {
 
-    var ToManyStore = declare([PersistentObjectStore, Stateful], {
+    var ToManyStore = declare([PersistentObjectStore, IdentifiableObject, Stateful], {
       // summary:
       //   Instances define a to-many relationship, expressed as a bidirectional
       //   one-to-many association, from objects of a given subtype of PersistentObject
@@ -34,6 +40,8 @@ define(["dojo/_base/declare", "./PersistentObjectStore", "dojo/Stateful", "ppwco
       _c_invar: [
         function() {return this._c_prop_mandatory("serverPropertyName");},
         function() {return this._c_prop_string("serverPropertyName");},
+        function() {return this._c_prop_mandatory("one");},
+        function() {return this.get("one").isInstanceOf && this.get("one").isInstanceOf(PersistentObject);},
         function() {return this._c_prop("lastReloaded");} // TODO date
       ],
 
@@ -42,6 +50,10 @@ define(["dojo/_base/declare", "./PersistentObjectStore", "dojo/Stateful", "ppwco
       //   in the server. A UrlBuilder will be asked to form a URL to load the associated
       //   many objects based on this name and the object that has the to-many association.
       serverPropertyName: null,
+
+      // one: PersistentObject
+      //   The one-instance.
+      one: null,
 
       // lastReloaded: Date?
       //   The time of last reload. Null if never reloaded. Should only be set by CrudDao.retrieveToMany.
@@ -52,17 +64,28 @@ define(["dojo/_base/declare", "./PersistentObjectStore", "dojo/Stateful", "ppwco
       _arbiter: null,
 
       constructor: function(/*Object*/ kwargs) {
-        this._c_pre(function() {return kwargs && kwargs.serverPropertyName && js.typeOf(kwargs.serverPropertyName) === "string";});
+        this._c_pre(function() {return kwargs;});
+        this._c_pre(function() {return kwargs.serverPropertyName && js.typeOf(kwargs.serverPropertyName) === "string";});
+        this._c_pre(function() {
+          return kwargs.one && kwargs.one.isInstanceOf && kwargs.one.isInstanceOf(PersistentObject);
+        });
 
+        this.one = kwargs.one;
         this.serverPropertyName = kwargs.serverPropertyName;
         this._arbiter = new Arbiter();
       },
 
+      getKey: function() {
+        return this.getTypeDescription() + "@" + this.one.getKey() + "[" + this.serverPropertyName + "]";
+      },
+
       toString: function() {
-        return "ToManyDefinition{" + this.serverPropertyName + ": " + this.inherited(arguments) + "}";
+        return this.getKey() + "{" + this.serverPropertyName + ": " + this.inherited(arguments) + "}";
       }
 
     });
+
+    ToManyStore.mid = module.id;
 
     return ToManyStore; // return ToManyStore
   }
