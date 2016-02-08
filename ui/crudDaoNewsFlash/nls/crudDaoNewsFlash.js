78952ef4804def484f2e28e9583fe73a9247be28
Jan Dockx <Jan_Dockx@peopleware.be>
2016-02-08 12:39:05 +0100

    Styles messages for CrudDaoNewsFlash.

--- ui/crudDaoNewsFlash/nls/crudDaoNewsFlash.js
+++ ui/crudDaoNewsFlash/nls/crudDaoNewsFlash.js
@@ -1,37 +1,48 @@
 define({
   root:{
     "DELETE":
-      "${this.getLabel({formatLength: 'long', locale: 'nl'})}$ is verwijderd. Het venster werd gesloten.",
+      "<p><span class='objectLabel'>${this.getLabel({formatLength: 'long', locale: 'nl'})}$</span> is verwijderd.</p>" +
+      "<p>Het venster werd gesloten.</p>",
 
     "POST":
-      "${this.getLabel({formatLength: 'long', locale: 'nl'})}$ is succesvol aangemaakt.",
+      "<span class='objectLabel'>${this.getLabel({formatLength: 'long', locale: 'nl'})}$</span> is succesvol " +
+      "aangemaakt.",
 
     "PUT":
-      "De nieuwe waarden voor ${this.getLabel({formatLength: 'long', locale: 'nl'})}$ zijn opgeslagen.",
+      "De nieuwe waarden voor <span class='objectLabel'>${this.getLabel({formatLength: 'long', locale: 'nl'})}$</span> " +
+      "zijn opgeslagen.",
 
     "ppwcode-vernacular-persistence/IdNotFoundException":
-      "${this.getLabel({formatLength: 'long', locale: 'nl'})}$ bestaat niet meer. Het venster werd gesloten.",
+      "<p><span class='objectLabel'>${this.getLabel({formatLength: 'long', locale: 'nl'})}$</span> bestaat niet " +
+      "meer.</p>" +
+      "<p>Het venster werd gesloten.</p>",
 
     "ppwcode-vernacular-persistence/ObjectAlreadyChangedException":
-      "${this.subject.getLabel({formatLength: 'long', locale: 'nl'})}$ is ondertussen reeds in een ander venster " +
-      "of door een andere gebruiker aangepast. Bekijk of uw wijzigingen nog nodig zijn, en herhaal ze indien nodig.",
+      "<p><span class='objectLabel'>${this.subject.getLabel({formatLength: 'long', locale: 'nl'})}$</span> is " +
+      "ondertussen reeds in een ander venster of door een andere gebruiker aangepast.</p>" +
+      "<p>Bekijk of uw wijzigingen nog nodig zijn, en herhaal ze indien nodig.</p>",
 
     "ppwcode-vernacular-exceptions/SemanticException[NOT UNIQUE]":
-      "Een waarde voor ${this.subject.getLabel({formatLength: 'long', locale: 'nl'})}$ die uniek moet zijn, " +
-      "is niet uniek. Pas de gegevens aan, en probeer opnieuw.",
+      "<p>Een waarde voor " +
+      "<span class='objectLabel'>${this.subject.getLabel({formatLength: 'long', locale: 'nl'})}$</span> die uniek moet " +
+      "zijn, is niet uniek.</p>" +
+      "<p>Pas de gegevens aan, en probeer opnieuw.</p>",
 
     "ppwcode-vernacular-exceptions/SemanticException":
-      "De ingevulde waarden voor ${this.subject.getLabel({formatLength: 'long', locale: 'nl'})}$ worden door de " +
-      "server niet aanvaard " + "(${(this.exception.key ? this.exception.key : '') + " +
-      "(this.exception.cause ? JSON.stringify(this.exception.cause) : ''}$). Pas de gegevens aan, en probeer opnieuw.",
+      "<p>De ingevulde waarden voor " +
+      "<span class='objectLabel'>${this.subject.getLabel({formatLength: 'long', locale: 'nl'})}$</span> worden door " +
+      "de server niet aanvaard.</p>" +
+      "<p>(<code>${(this.exception.key ? this.exception.key : '') + (this.exception.cause ? JSON.stringify(this.exception.cause) : '')}$</code>)</p>" +
+      "<p>Pas de gegevens aan, en probeer opnieuw.</p>",
 
     "ppwcode-vernacular-exceptions/SecurityException":
       "De gevraagde gegevens zijn voor u niet toegankelijk.",
 
     "error":
-      "Er heeft zich een onverwachte situatie voorgedaan. We verontschuldigen ons, maar uw " +
-      "laatste wijzigingen zijn niet opgeslagen. " +
-      "Herlaad de pagina, en probeer opnieuw. Indien de fout zich " +
-      "blijft herhalen, kan u ons contacteren. (${this.exception.message || this.exception.toString()}$)"
+      "<p>Er heeft zich een onverwachte situatie voorgedaan. We verontschuldigen ons, maar uw laatste wijzigingen " +
+      "zijn niet opgeslagen.</p>" +
+      "<p>Herlaad de pagina, en probeer opnieuw.</p>" +
+      "<p>Indien de fout zich blijft herhalen, kan u ons contacteren.</p>" +
+      "<pre>(${this.exception.message || this.exception.toString()}$)</pre>"
   }
 });
